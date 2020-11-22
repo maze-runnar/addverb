53e07434a51d43351c9c4ff75cecfed1a593ca00
sundaram <dubeysundaram057@gmail.com>
2020-11-22 16:25:27 +0530

    it sucks

--- api/api.py
+++ api/api.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, jsonify, make_response
+from flask import Blueprint, jsonify, make_response, session
 from flask_restful import reqparse, request
 from api import db
 from api.models.models import (
-    Student
+    Student,
+    MySchedule,
+    Attendance
 )
 
 api_blueprint = Blueprint('api','api', url_prefix='/api')
@@ -34,9 +36,83 @@ def RegisterStudent():
     return make_response(
         jsonify({
             "status": 'success',
+            "logged_in": True,
             "data": {
                 'email': email,
                 'password': password
             }
         })
     )
+
+def AllStudents():
+	allstudents = db.session.query(Student).all()
+	db.session.commit()
+	return make_response(
+        jsonify({
+            "allstudents": allstudents
+        })
+    )
+
+def Logout():
+	session['logged_in'] = False
+	session["teacher_logged_in"] = False
+	return make_response(
+        jsonify({
+            "logged_in": False
+        })
+    )
+
+
+def CurrentClass():
+    allclasses = db.session.query(MySchedule).all()
+    upcoming_classes = []
+    previous_classes = []
+    for i in allclasses:
+        t = i.time.split("T")
+        x = str(datetime.utcnow()).split(" ")
+        current_time_hour = x[1][0:2]
+        current_time_min = x[1][3:5]
+        current_time_date = x[0]
+        class_time_hour = t[1][0:2]
+        class_time_min = t[1][3:5]
+        class_time_date = t[0]
+        if(current_time_hour > class_time_hour or class_time_date < current_time_date):
+            previous_classes.append(i)
+        else:
+            upcoming_classes.append(i)
+
+    return make_response(
+        jsonify({
+        "previous_classes": previous_classes,
+        "upcoming_classes": upcoming_classes
+        })
+        )
+
+
+def attendance(id):
+	if request.method == "POST":
+		my_subject = db.session.query(MySchedule).filter_by(id = id).first()
+		my_subject_name = my_subject.subject
+		url = my_subject.url
+		std_email = session["student_email"]
+		new_attendee = Attendance(email=std_email, subject=my_subject_name)
+		try:
+			db.session.add(new_attendee)
+			db.session.commit()
+			return make_response(
+                jsonify({
+                    "status": "success",
+                    data:{
+                        "meeting-url": url
+                    }
+                })
+            )
+		except:
+			return "error is here"
+	else:
+		return make_response(
+            jsonify({
+                "get_current_class": "/current-class"
+            })
+        )
+	
--- api/routes.py
+++ api/routes.py
@@ -1,8 +1,16 @@
 from api.api import  (
     api_blueprint,
     index,
-    RegisterStudent
+    RegisterStudent,
+    AllStudents,
+    Logout,
+    CurrentClass,
+    attendance
 )
 
 api_blueprint.add_url_rule('/', methods=['GET'], view_func=index)
 api_blueprint.add_url_rule('/registerstudent', methods=['POST'], view_func=RegisterStudent)
+api_blueprint.add_url_rule('/allstudents', methods=['GET', 'POST'], view_func=AllStudents)
+api_blueprint.add_url_rule('/logout', methods=["POST", "GET"], view_func=Logout)
+api_blueprint.add_url_rule('/current-class', methods=["POST", "GET"], view_func=CurrentClass)
+api_blueprint.add_url_rule('/attendance/<int:id>', methods=["GET", "POST"], view_func=attendance)
